d5dfcf9b6f2e6841a72501b1476b48a1e0377807
Saint Wish <SaintWish@users.noreply.github.com>
2021-01-09 18:26:12 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # auralog
-A Go logging package based off Go's Log package with additional features.
+A Go logging package based off Go's Log package with some differences. This has about as much overhead as the stock Log package because it doesn't use thirdparty packages like the stock package. The differences it has over the stock Go log library are below:
+* Different logging levels ``INFO, WARN, ERROR, FATAL, PANIC``
+* Config struct for cleaner initialization.
+* Removed helpers so it has to be manually.
+* Built in Rotate Writer io.Writer interface for basic log file rotation.
+
+## Getting Started
+``go get github.com/saintwish/auralog`` Will get the latest from master branch.
+
+## Documentation
+Nothing here yet.
+
+## Example
+This is a basic example of how you will initialize the logger and use it with the RotateWriter.
+```go
+package main
+
+import(
+  "os"
+  "io"
+  "time"
+
+  "github.com/saintwish/auralog"
+)
+
+var (
+  flags = auralog.Ldate | auralog.Ltime
+  wflags = auralog.Ldate | auralog.Ltime
+  eflags = auralog.Ldate | auralog.Ltime | auralog.Lshortfile
+  dflags = auralog.Ldate | auralog.Ltime | auralog.Lshortfile
+
+  log *auralog.Logger
+)
+
+func main() {
+  file := &auralog.RotateWriter{
+    Dir: "./runtime/logs/", //Required
+    Filename: "log.log", //Required
+    ExTime: 24 * time.Hour, //Required if you want daily log rotation.
+    MaxSize: 5 * auralog.Megabyte, //Can also use auralog.Kilobyte, Not required
+  }
+
+  log = auralog.New(auralog.Config{
+    Output: io.MultiWriter(file, os.Stdout), //Required
+    Prefix: "[PREFIX] ", //Not Required
+    Level: auralog.LogLevelInfo, //Required
+    Flag: flags, //Required
+    WarnFlag: wflags, //Required
+    ErrorFlag: eflags, //Required
+    DebugFlag: dflags, //Required
+  })
+  log.Println("Test")
+  log.Warnln("TEST WARN")
+  log.Errorln("TEST ERROR")
+  log.Debugln("TEST DEBUG")
+}
+```
+
+## Contributing
+If you have any additions or contributions you would like to make please do. Just keep the below in mind.
+* Try to match current naming conventions as closely as possible.
+* Create a Pull Request with your changes against the master branch.
+
+## License
+It's licensed under BSD-2-Clause License to stay compatible with Go's stock log package, since it's created from that.
---
+++ rotatewriter.go
@@ -0,0 +1,115 @@
+// Credits to https://stackoverflow.com/a/28797984
+package auralog
+
+import (
+  "os"
+  "sync"
+  "time"
+  "strings"
+  "path/filepath"
+)
+
+var (
+  Megabyte int64 = 1024 * 1024
+  Kilobyte int64 = 1024
+)
+
+type RotateWriter struct {
+  Dir string
+  Filename string // should be set to the actual filename
+  ExTime time.Duration
+  MaxSize int64
+
+  lock sync.Mutex
+  now time.Time
+  fp *os.File
+}
+
+// Write satisfies the io.Writer interface.
+func (w *RotateWriter) Write(output []byte) (int, error) {
+  w.lock.Lock()
+  defer w.lock.Unlock()
+
+  if w.fp == nil {
+    if err := w.Resume(); err != nil {
+      return 0, err
+    }
+  }
+
+  if w.now.After(w.now.Add(w.ExTime)) {
+    if err := w.Rotate(); err != nil {
+      return 0, err
+    }
+  }
+
+  return w.fp.Write(output)
+}
+
+func (w *RotateWriter) Resume() error {
+  var err error
+  var filename = w.Dir+w.Filename
+
+  // Check if file already exixts to resume it.
+  if _, err := os.Stat(filename); err != nil {
+    return err
+  }
+
+  w.now = time.Now()
+  w.fp, err = os.OpenFile(filename, os.O_RDWR | os.O_CREATE, 0666)
+  return err
+}
+
+// Perform the actual act of rotating and reopening file.
+func (w *RotateWriter) Rotate() error {
+  var err error
+  var filename = w.Dir+w.Filename
+
+  // no need to lock if it's being locked anyways by the caller.
+  // w.lock.Lock()
+  // defer w.lock.Unlock()
+
+  // create the needed direactories if they don't exists.
+  if err := os.MkdirAll(w.Dir, 0755); err != nil {
+    return err
+  }
+
+  // Close existing file if open
+  if w.fp != nil {
+    if err := w.fp.Close(); err != nil {
+      return err
+    }
+    w.fp = nil
+  }
+
+  // Rename dest file if it already exists
+  if fi, err := os.Stat(filename); err == nil {
+    if err := w.renameFile(); err != nil {
+      return err
+    }
+  }else{
+    if w.MaxSize > 0 {
+      if fi.Size() >= w.MaxSize {
+        if err := w.renameFile(); err != nil {
+          return err
+        }
+      }
+    }
+  }
+
+  // Create a file.
+  w.now = time.Now()
+  w.fp, err = os.OpenFile(filename, os.O_RDWR | os.O_CREATE, 0666)
+  return err
+}
+
+func (w *RotateWriter) renameFile() error {
+  var filename = w.Dir+w.Filename
+  newfn := filename[:len(filename)-len(filepath.Ext(w.Filename))]+"-"+time.Now().Format(time.RFC3339)+filepath.Ext(w.Filename)
+  err := os.Rename(filename, cleanName(newfn));
+  return err
+}
+
+// Replace : to _ for os.Rename.
+func cleanName(name string) string {
+  return strings.ReplaceAll(name, ":", "_")
+}
